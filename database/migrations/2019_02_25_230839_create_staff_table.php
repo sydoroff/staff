696b6218a2aaecb7c501305930898a96ac3056e2
sydoroff <is.sydoroff@gmail.com>
2019-02-27 23:46:09 +0200

    Задание №2.1 Создайте базу данных используя миграции Laravel

---
+++ database/migrations/2019_02_25_230839_create_staff_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('position')->nullable();
+            $table->date('employment');
+            $table->decimal('pay')->nullable();
+            $table->integer('up_num')->index()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
